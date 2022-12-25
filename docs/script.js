11ff469a15443386e2d243e79eeaee2822c6eba6
Hex09AF <lsqhuy@gmail.com>
2022-12-25 23:03:33 +0700

    Add animation gift

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,14 @@
-var btnNext = document.getElementById("btn-next")
-var body = document.getElementById("root");
-btnNext.addEventListener("click", function(event) {
+const btnNext = document.getElementById("btn-next")
+const body = document.getElementById("root");
+const listBtnSlide = document.getElementsByClassName("btn-slide-dot");
+let idx = 0;
+
+listBtnSlide[idx].classList.toggle("active");
+
+btnNext.addEventListener("click", (event) => {
     event.preventDefault();
+    listBtnSlide[idx].classList.toggle("active");
+    idx = (idx + 1) % 2;
+    listBtnSlide[idx].classList.toggle("active");
     body.classList.toggle("step-2");
 })
