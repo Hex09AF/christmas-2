6d752b09d148a5da038af2fc07e3b17e5867e0da
Hex09AF <lsqhuy@gmail.com>
2022-12-26 19:25:59 +0700

    Add more image and animation, fix logic click button slide

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,18 @@
 const btnNext = document.getElementById("btn-next")
 const body = document.getElementById("root");
-const listBtnSlide = document.getElementsByClassName("btn-slide-dot");
+const listBtnSlide = [...document.getElementsByClassName("btn-slide-dot")];
 let idx = 0;
+
+listBtnSlide.map((v, index) => {
+    v.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (idx == index) return;
+        listBtnSlide[idx].classList.toggle("active");
+        idx = index;
+        listBtnSlide[idx].classList.toggle("active");
+        body.classList.toggle("step-2");
+    })
+});
 
 listBtnSlide[idx].classList.toggle("active");
 
